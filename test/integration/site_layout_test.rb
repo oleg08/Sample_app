480beacf3e586cc4e839889dea424fc46faffd44
Oleg Hudak <olhudak@gmail.com>
2015-11-01 23:01:20 +0200

    9.6

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,17 +1,42 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-  test "layout links" do
+
+  def setup
+    @user = users(:archer)
+  end
+
+  test "layout links if logged_in" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
     get signup_path
     assert_select "title", full_title("Sign up")
   end
+
+  test "layout links if not logged_in" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", edit_user
+    assert_select "a[href=?]", current_user
+    assert_select "a[href=?]", logout_path
+
+
+    get signup_path
+    assert_select "title", full_title("Sign up")
+  end
+
 end
