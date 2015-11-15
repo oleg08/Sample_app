6082cf0d14b9495ca25a145b9f92cb69a3a2918f
Oleg Hudak <olhudak@gmail.com>
2015-11-15 23:39:05 +0200

    11.3.3

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,16 +5,16 @@ class StaticPagesControllerTest < ActionController::TestCase
     @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
-  test "should get home" do
+  test 'hould get home' do
     get :home
     assert_response :success
-    assert_select "title"" | #{@base_title}"
+    assert_select 'title'" | #{@base_title}"
   end
 
-  test "should get help" do
+  test 'should get help' do
     get :help
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select 'title', "Help | #{@base_title}"
   end
 
   test 'should get about' do
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,7 +6,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     @user = users(:archer)
   end
 
-  test "layout links if  not logged_in" do
+  test 'layout links if  not logged_in' do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -32,7 +32,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", signup_path, count: 0
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", users_path
     assert_select "a[href=?]", edit_user_path(@user)
