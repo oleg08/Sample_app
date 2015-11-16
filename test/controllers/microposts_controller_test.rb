0da3ed17338cd5b2e30954bfe612cf3818508135
Oleg Hudak <olhudak@gmail.com>
2015-11-16 10:52:23 +0200

    11.3

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -7,15 +7,24 @@ class MicropostsControllerTest < ActionController::TestCase
 
   test "should redirect create when not logged in" do
     assert_no_difference 'Micropost.count' do
-      post :create, micropost: { content: "Lorem ipsum" }
+      post :create, micropost: { content: 'Lorem ipsum' }
     end
     assert_redirected_to login_url
   end
 
-  test "should redirect destroy when not logged in" do
+  test 'should redirect destroy when not logged in' do
     assert_no_difference 'Micropost.count' do
       delete :destroy, id: @micropost
     end
     assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy for wrong micropost' do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete :destroy, id: micropost
+    end
+    assert_redirected_to root_url
   end
 end
